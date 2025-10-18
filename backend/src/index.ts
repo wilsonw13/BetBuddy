1959cdf771b0849b03bbf95c1aa3b7e0fe03ac0c
Chang <61592337+ZeoShadow@users.noreply.github.com>
2025-10-18 19:57:49 -0400

    small edits

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,7 +28,7 @@ const server = new ApolloServer({
 
 const main = async () => {
   // seed mock data
-  await seedDatabase();
+  await seedDatabase(false);
 
   const { url } = await startStandaloneServer(server, {
     listen: { host: HOST, port: PORT },
